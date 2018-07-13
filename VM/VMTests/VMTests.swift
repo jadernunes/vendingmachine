65772e8348327919af3aa93fa1d98a53ccda7f84
Jader Nunes <jadernunes.jbn@gmail.com>
2018-07-13 16:56:15 -0700

    created a unit test validating the function add amount

--- VM/VMTests/VMTests.swift
+++ VM/VMTests/VMTests.swift
@@ -9,28 +9,27 @@
 import XCTest
 @testable import VM
 
+let timeout = 30.0
+
 class VMTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAddAmount(){
+        let expec = self.expectation(description: #function)
+        
+        let amount = 10
+        Money.shared.addAmount(amount: amount) { _,_  in
+            expec.fulfill()
+            XCTAssertEqual(amount, 10)
         }
+        
+        waitForExpectations(timeout: timeout, handler: nil)
     }
-    
 }
